4c53713703797a757add15887b3a9ff9259e0674
Alvaro Pastor-Sanchez <a.pastor.sanchez@gmail.com>
2024-09-15 20:10:16 +0100

    Update test_domain_io_isolation.py

--- test_domain_io_isolation.py
+++ test_domain_io_isolation.py
@@ -1,7 +1,7 @@
 """
-Pure Domain code should not contain dependencies to IO modules. The reason for this is 
-that we would like the data processing separate from the handling of this data to 
-external entities (files, dbs, the internet...). This helps with modularity and also 
+Pure Domain code should not contain dependencies to IO modules. The reason for this is
+that we would like the data processing separate from the handling of this data to
+external entities (files, dbs, the internet...). This helps with modularity and also
 with unit testing.
 
 The IO modules should be the ones offering what the Domain requires in terms of data
@@ -13,7 +13,7 @@ from itertools import product
 from pathlib import Path
 from typing import Iterable, TypeVar
 
-import pytest
+from pytest import mark
 
 
 ROOT = Path(__file__).parents[2].resolve()
@@ -52,28 +52,20 @@ class TestDomainCodeDoesNotImportIO:
     DOMAIN_MODULES = {"api.core", "api.predicates", "api.query"}
     IO_MODULES = {"api.file", "api.main"}
 
-    @pytest.mark.parametrize(
-        "domain_module, io_module", product(DOMAIN_MODULES, IO_MODULES)
-    )
+    @mark.parametrize("domain_module, io_module", product(DOMAIN_MODULES, IO_MODULES))
     def test_import(self, domain_module: str, io_module: str):
-        statements = iter_statements(domain_module, statement_type=ast.Import)
+        imports = iter_statements(domain_module, statement_type=ast.Import)
 
-        if any(
+        assert not any(
             imported.name == io_module
-            for import_statement in statements
-            for imported in import_statement.names
-        ):
-            pytest.fail(reason=DomainImportError(io_module, domain_module).message)
+            for import_ in imports
+            for imported in import_.names
+        ), DomainImportError(io_module, domain_module).message
 
-    @pytest.mark.parametrize(
-        "domain_module, io_module", product(DOMAIN_MODULES, IO_MODULES)
-    )
+    @mark.parametrize("domain_module, io_module", product(DOMAIN_MODULES, IO_MODULES))
     def test_import_from(self, domain_module: str, io_module: str):
-        statements = iter_statements(domain_module, statement_type=ast.ImportFrom)
+        import_froms = iter_statements(domain_module, statement_type=ast.ImportFrom)
 
-        if any(
-            imported_from
-            for imported_from in statements
-            if imported_from.module == io_module
-        ):
-            pytest.fail(reason=DomainImportError(io_module, domain_module).message)
+        assert not any(
+            imported.module == io_module for imported in import_froms
+        ), DomainImportError(io_module, domain_module).message
